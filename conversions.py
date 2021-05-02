0bd062d0fed04cc8c6a762d5e56fe0aea1c047d3
Christos Zonios <czonios@gmail.com>
2021-05-02 18:02:25 +0300

    Adds documentation

--- conversions.py
+++ conversions.py
@@ -1,16 +1,20 @@
 import math
 
 class SpeedConversion():
+    """Contains modules and helpers for converting meters per second to min/km or min/mile
+    """
     def __init__(self):
         pass
 
     def convert(self, valueInMeters, unit="metric"):
+        # conditionally converts from metric to imperial
         if unit == "imperial":
             return valueInMeters * 0.621371
         else:
             return valueInMeters
 
     def format(self, valueInMPS, unit="metric"):
+        # converts and formats a meters/sec value into running pace
         converted = self.convert(valueInMPS, unit)
         minper = 16.666666667 / converted
         hours = math.floor(minper / 60)
@@ -23,18 +27,36 @@ class SpeedConversion():
             return str(minutes) + ":" + ("0" if seconds < 10 else "") + str(seconds)
 
     def getMinPerKm(self, mps, showUnit=True):
+        """Converts m/s to running pace
+
+        Args:
+            mps (float): speed in meters per second
+            showUnit (bool, optional): whether to also show unit. Defaults to True.
+
+        Returns:
+            str: speed in min/km
+        """
         unit = " min/km"
         unitStr = unit if showUnit else ""
-
         return f"{self.format(mps, unit='metric')}{unitStr}"
 
     def getMinPerMile(self, mps, showUnit=True):
+        """Converts m/s to running pace
+
+        Args:
+            mps (float): speed in meters per second
+            showUnit (bool, optional): whether to also show unit. Defaults to True.
+
+        Returns:
+            str: speed in min/mile
+        """
         unit = " min/mile"
         unitStr = unit if showUnit else ""
-
         return f"{self.format(mps, unit='imperial')}{unitStr}"
 
 class DistanceConversion():
+    """Contains modules and helpers for converting meters to km or miles
+    """
     def __init__(self):
         pass
 
@@ -53,8 +75,25 @@ class DistanceConversion():
             math.pow(10, roundTo)), roundTo
         )
 
-    def meters2miles(self, meters, showUnit=True):
+    def getMiles(self, meters, showUnit=True):
+        """Converts meters to miles
+
+        Args:
+            meters (float): distance in meters
+            showUnit (bool, optional): whether to also show unit. Defaults to True.
+
+        Returns:
+            str: distance in miles
+        """
         unit = " miles"
         unitStr = unit if showUnit else ""
-
         return f"{self.round(meters)}{unitStr}"
+
+# create instances of classes
+distConversion = DistanceConversion()
+speedConversion = SpeedConversion()
+
+# export functions as standalone for minimal boilerplate
+getMiles = distConversion.getMiles
+getMinPerKm = speedConversion.getMinPerKm
+getMinPerMile = speedConversion.getMinPerMile
